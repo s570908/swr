d2abe1b02ff1c620857a44a967dbeb13a370ac6a
s570908 <songohyoung@gmail.com>
2022-11-23 12:29:43 +0900

    getObj를 recursive function 으로 구현하였다. 이전 commit에서 구현하였었고 bug라고 판단하였으나 bug가 아님을 이번 commit에서 확인하였다.

--- examples/local-state-sharing/utils/getObj.js
+++ examples/local-state-sharing/utils/getObj.js
@@ -1,30 +1,12 @@
-function getObj(obj, prop, val) {
-	const props = prop.split('.');
-	const final = props.pop(); // 마지막 프로퍼티는 final로 뽑아 놓는다.
-	let p;
-	while(p = props.shift()){
-		if (typeof obj[p] === 'undefined') {
-			// If we're setting
-			if (typeof val !== 'undefined') {
-				// If we're not at the end of the props, keep adding new empty objects
-				obj[p] = {};
-			}
-			else
-				return undefined;
-		}
-		obj = obj[p]
-	}
-
-	// val 이 주어지지 않았다면 get이다.
-	// 현재 obj 는 마지막 object이고 {final: <<someValue>>} 이다.
-	//
-	// val 이 주어졌다면 set이다.
-	// 현재 obj 는 마지막 object이고 {final: <<someValue>>}  혹은 {} 이다.
-	//
-	// val 이 주어졌을 경우, 
-	// 	obj 가  {final: <<someValue>>} 라면 obj는 {final: value}  로 업데이트된다.
-	// 	obj가 {} 이라면 {final: value} 로 내용을 만들어서 넣는다.
-	return val ? (obj[final] = val) : obj[final]
+function index(obj,is, value) {
+	if (typeof is === 'string')
+			return index(obj,is.split('.'), value);
+	else if (is.length===1 && value!==undefined)
+			return obj[is[0]] = value;
+	else if (is.length===0)
+			return obj;
+	else
+			return index(obj[is[0]],is.slice(1), value);
 }
 
-export default getObj;
+export default index;
